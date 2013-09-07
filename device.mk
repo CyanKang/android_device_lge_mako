8fe50f48b0ab87bfc68094d049f3340df6cbec66
Hendrik H <finnq@finnq.de>
2013-09-07 21:38:06 +0200

    Don't compile power.msm8960

--- device.mk
+++ device.mk
@@ -189,8 +189,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
 	hci_qcomm_init
 
-PRODUCT_PACKAGES += \
-	power.msm8960
+#PRODUCT_PACKAGES += \
+#	power.msm8960
 
 PRODUCT_COPY_FILES += \
 	device/lge/mako/init.mako.bt.sh:system/etc/init.mako.bt.sh
